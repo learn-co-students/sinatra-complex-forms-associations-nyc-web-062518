0912023b6999c1245af0840bcf8ee0bb05aacbf8
Daniel Ran <danielr1735@gmail.com>
2018-07-18 23:21:43 -0400

    solved edit user issue

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -30,8 +30,13 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do
-    binding.pry
+    # binding.pry
     @owner = Owner.find(params[:id])
+    #########
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+    #######
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
